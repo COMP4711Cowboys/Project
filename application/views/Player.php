7894cc88911430e2c1f212457187c6ec6741f161
Derek <Derek@192.168.200.128>
2015-11-13 15:25:06 -0800

    Created player view for displaying players individually; using db field names

---
+++ application/views/Player.php
@@ -0,0 +1,31 @@
+<div>
+    <div class="player-image"><img src="img/mugs/profile_01.png" title="{surname}"/></div>
+    <div class="player-desc">
+        <table>
+            <tr>
+                <th>Name:</th>
+                <td>{firstname} {surname}</td>
+            </tr>
+            <tr>
+                <th>Jersey #:</th>
+                <td>{jersey}</td>
+            </tr>
+            <tr>
+                <th>Position:</th>
+                <td>{position}</td>
+            </tr>
+            <tr>
+                <th>Age:</th>
+                <td>{age}</td>
+            </tr>
+            <tr>
+                <th>Weight:</th>
+                <td>{weight} lbs</td>
+            </tr>
+            <tr>
+                <th>College:</th>
+                <td>{college}</td>
+            </tr>
+        </table>
+    </div>
+</div>
