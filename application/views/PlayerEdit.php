5b6b6620578c77a98b1e92b9c4ab5493fa0dafff
Derek <Derek@192.168.200.128>
2015-11-15 09:05:39 -0800

    Created initial player edit view; need to make it work with player controller

---
+++ application/views/PlayerEdit.php
@@ -0,0 +1,12 @@
+<div>
+    <form action="/player/confirm" method="post">
+        {f_firstname}
+        {f_surname}
+        {f_jersey}
+        {f_position}
+        {f_age}
+        {f_weight}
+        {f_college}
+        {f_filename}
+    </form>
+</div>
