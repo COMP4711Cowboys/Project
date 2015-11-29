5760c7b8615795aedb6c5805121767538c41c28c
Jathavan <school@krusnix.com>
2015-11-29 15:40:10 -0800

    99% complete history model

--- application/models/History.php
+++ application/models/History.php
@@ -6,26 +6,30 @@
  * @author Jathavan Arumugam
  */
 define('LOCAL', false);   // control whether we access our model locally, or over XML-RPC
-define('RPCSERVER', ('nfl.jlparry.com/rpc/since')); // endpoint fo the XML-RPC server
+define('RPCSERVER', ('nfl.jlparry.com/rpc')); // endpoint fo the XML-RPC server
 define('RPCPORT', 80); // port the XML-RPC service is listening on
 define('DATAPATH', ('./assets/')); // port the XML-RPC service is listening on
 
 
-class History extends MY_Model {
+class History extends MY_Model2 {
 
     protected $xml = null;
     
     public function __construct(){
-        parent::__construct('home', 'away', 'score', 'date', 'submitted');
-
+        parent::__construct('history', 'home', 'date');
     }
 
-    public function get_data(){
+    //Recieves the XML from the remote server and adds records to the database.
+    public function get_data_from_remote(){
         
         $this->xmlrpc->server(RPCSERVER, RPCPORT);
-        $this->xmlrpc->method('scores');
+        $this->xmlrpc->method('since');
 
-        $request = array('20150830');
+        $mostRecentUpdate = $this->get_update_date();
+
+        //$mostRecentUpdate = "20150830"; 
+        $request = array($mostRecentUpdate);
+        $this->get_update_date();
         $this->xmlrpc->request($request);
 
         if (!$this->xmlrpc->send_request())
@@ -35,54 +39,85 @@ class History extends MY_Model {
         }
 
         $list = $this->xmlrpc->display_response();
-        
-        var_dump($list);
+        $this->parse_result($list);
+        print_r($list);
         die();
-
     }
 
+    //Parses a local XML file and adds records to the database
     public function parse_xml() {
         $this->xml = simplexml_load_file(DATAPATH . 'results.xml');
 
         foreach($this->xml->game as $game){
-            //echo ($game);
-            $record = $this->extract($game);
-
-            $this->add($record);
-            echo($record);
-           /** echo($record->away);
-            echo($record->score);
-            echo($record->date);
-            echo($record->submitted);**/
-
-            die();
+            $record = $this->extract_xml($game);
+            $this->add_game($record);
         }
+    }
 
+    //Parse the list returned by the XML-RPC request
+    public function parse_result($list) {
 
-        //var_dump($this->xml);
+        foreach($list as $game){
+            $record = $this->extract($game);
+            $this->add_game($record);
+        }
+    }
+
+    //Adds the game to the database
+    public function add_game($record){
+        $gameExists = $this->exists($record->home, $record->date);
+
+        if(!$gameExists){
+            $this->add($record);
+        } 
 
     }
 
+    //Retrieve the day the record was updated on
+    function get_update_date(){
+        $result = $this->all();
+
+        if($result == NULL){
+            return "20150830";
+        }
+        
+        $lastUpdate = NULL;
+        foreach($result as $dates)
+        {
+            foreach($dates as $element => $date)
+            {
+                if ($element == "inserted" && ($date >= $lastUpdate))
+                {
+                    $lastUpdate = $date;
+                }
+            }
+        }
+        $lastUpdate = new DateTime($lastUpdate);
+        $result = $lastUpdate->format('Ymd');
+        return($result);
+    }
+
+    //Extracts the game from the XML File and creates a history record from it
     function extract($element){
-        $record = new stdClass();
-        $record->number = (string) $element['number'];
+        $record = $this->create();
+        $record->date = $element['date'];
+        $record->away = $element['away'];
+        $record->home = $element['home'];
+        $record->score = "21:21";
+        $record->inserted = Date("Y-m-d H:i:s");
+        return $record; 
+    }
+
+    //Extracts the game from the XML File and creates a history record from it
+    function extract_xml($element){
+        $record = $this->create();
         $record->date = (string) $element['date'];
         $record->away = (string) $element['away'];
         $record->home = (string) $element['home'];
-
         $record->score = (string) $element;
+        $record->inserted = Date("Y-m-d H:i:s");
 
-
-        $record->game = (string) $element;
-
-        $game = new History($record->home, $record->away, $record->score, $record->date, Date("Ymd"));
-
-        return $game; 
-
-
-        //parent::__construct('home', 'away', 'score', 'date', 'submitted');
-
-        //$game = new History($element[])
+        return $record; 
     }
 
 
