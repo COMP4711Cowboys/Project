774ee03fb240a7322f4f3392270232d0de323b07
FriendlyNPC <Devanyim@gmail.com>
2015-11-29 13:36:57 -0800

    work on prediction, still returns true always

--- application/controllers/Prediction.php
+++ application/controllers/Prediction.php
@@ -30,26 +30,21 @@ class Prediction extends Application {
                 ->set_content_type('text/html')
                 ->set_output("");
         } else {
+            
+            //get game records, returns true or false.
+            $win = $this->History->predictWin($encodedTeam);
+
             //get the name of the other team
-            $other_team = $this->League->getTeamName($encodedTeam);
+            $this->data["other_team"] = $this->League->getTeamName($encodedTeam);
 
-            //get game records
-            //$winner = $this->History->predictWinner($home, $encodedTeam);
-            $win = true;
-
-            //do prediction stuff
-            //$this->data["TeamCode"] = $winner;
-
-            //format the answer
-
-            $this->data["other_team"] = $other_team;
-
+            //get the html we're going to return
             if ($win) {
                 $winnerHTML = $this->parser->parse('_predictWin', $this->data, true);
             } else {
                 $winnerHTML = $this->parser->parse('_predictLoss', $this->data, true);
             }
 
+            //sent the OK response with the HTML payload
             $this->output
                 ->set_status_header(200)  //Status Code: OK
                 ->set_content_type('text/html')
--- application/models/History.php
+++ application/models/History.php
@@ -42,5 +42,93 @@ class History extends MY_Model {
 
     }
 
-
+    //determine if the team
+    public function predictWin($against) {
+        //our team code
+        $us = "DAL";
+        
+        //playing against ourself!? we win by default
+        if ( $us == $against ) {
+            return true;
+        }
+        
+        return true;
+        
+        //get our average
+        $our_average = $this->total_game_average($us);
+        $our_10_game_average =  $this->last_game_average($us,10);
+        $our_5_game_average_against = $this->last_game_average($us,$against, 5);
+        
+        $certainty = 0.7 * $our_average 
+                + 0.2 * $our_10_game_average 
+                + 0.1 * $our_5_game_average_against;
+        
+        if ($certainty > .5 ){
+            return true;
+        }
+        return false;
+    }
+    
+    private function total_game_average($team){
+        $query = $this->db->select('score', 'home', 'away')
+                ->where('home', $team)
+                ->or_where('away',$team);
+        
+        $results = $query->results();
+        $average = $this->calc_average($results, $team);
+        
+        return $average;
+    }
+    
+    private function last_game_average($team, $games){
+        if (!is_numeric($games) || !is_string($team)){
+            return 0;
+        }
+        $query = $this->db->select('score', 'home', 'away')
+                ->or_where('home', $team)
+                ->or_where('away',$team)
+                ->limit($games);
+        
+        $results = $query->results();
+        $average = $this->calc_average($results, $team);
+        
+        return $average;
+    }
+    
+    private function last_game_average_against($us, $them, $games){
+        if (!is_numeric($games) || !is_string($us) || !is_string($them)){
+            return 0;
+        }
+        
+        $query = $this->db->select('score', 'home', 'away')
+                ->where_in('home', array($us,$them))
+                ->where_in('away', array($us,$them))
+                ->limit($games);
+        
+        $results = $query->results();
+        $average = $this->calc_average($results, $team);
+        
+        return $average;
+    }
+    
+    private function calc_average($results, $team){
+        $total = 0;
+        $count = 0;
+        
+        foreach( $results as $rows ){
+            //split the scores home is 0, away is 1
+            $scores = explode(":",$row->score);
+            
+            if ($team == $row->home ){
+                $total = $total + intval($scores[0]);
+            } else if ($team == $row->away ){
+                $total = $total + intval($scores[1]);
+            }
+            $count = $count + 1;
+        }
+        $average = $total / $count;
+        
+        return $average;
+    }
+    
 }
